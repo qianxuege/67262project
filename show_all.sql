9e11efa1abb55cff8c7ee6d91d0a29046cf6318e
qianxuege <qianxuege15@gmail.com>
2024-11-22 22:22:55 -0500

    wrote show_all

--- show_all.sql
+++ show_all.sql
@@ -1,3 +1,21 @@
 from common import *
 
+def show_table( tablename ):
 
+        tmpl =  f'''
+    SELECT *
+    FROM %s
+    '''
+        cmd = cur.mogrify(tmpl, (tablename,))
+        print_cmd(cmd)
+        cur.execute(cmd)
+        rows = cur.fetchall()
+        pp(rows)
+        show_table( rows, cols) 
+
+def show_all():
+    tables = [Hotel_Provider, Hotel, Provision, Room]
+    for table in tables:
+        show_table(table)
+
+show_all()
