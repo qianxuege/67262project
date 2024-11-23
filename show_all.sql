ad95239f5faf9b5de4f120be7580b9fb9425938d
qianxuege <qianxuege15@gmail.com>
2024-11-22 22:45:21 -0500

    working on show_all

--- show_all.sql
+++ show_all.sql
@@ -1,10 +1,9 @@
 from common import *
 
 def show_table( tablename ):
-
         tmpl =  f'''
     SELECT *
-    FROM %s
+    FROM {tablename}
     '''
         cmd = cur.mogrify(tmpl, (tablename,))
         print_cmd(cmd)
@@ -14,7 +13,7 @@ def show_table( tablename ):
         show_table( rows, cols) 
 
 def show_all():
-    tables = [Hotel_Provider, Hotel, Provision, Room]
+    tables = ["Hotel_Provider", "Hotel", "Provision", "Room"]
     for table in tables:
         show_table(table)
 
