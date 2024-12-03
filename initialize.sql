726ca29b655e76ec7508034d0b3645f768338935
qianxuege <qianxuege15@gmail.com>
2024-12-02 20:02:26 -0500

    updating initialize

--- initialize.sql
+++ initialize.sql
@@ -1,7 +1,5 @@
--- drop the trax database if it exists
+-- drop the project database if it exists
 DROP database if EXISTS project;
-DROP table if EXISTS Hotel;
-DROP table if EXISTS Hotel;
 
 -- create it afresh
 CREATE database project;
@@ -10,12 +8,16 @@ CREATE database project;
 \i create.SQL
 
 -- load the data
-
-\copy Hotel(hotel_id, hotel_name, location, pool, rating, cheapest_nightly_rate) FROM data/hotel.csv csv header;
-\copy Room(room_number, hotel_id, room_capacity, nightly_rate, available) FROM data/room.csv csv header;
+\copy Aircraft(model_type, date_made, seating_capacity, wifi) FROM data/aircraft.csv csv header;
+-- \copy Flight(flight_id, destination, flight_price, departure_time, arrival_time, occupancy) FROM data/flight.csv csv header;
+\copy Traveler(user_id) FROM data/traveler.csv csv header;
+\copy Guest_Traveler(user_id, login_ip_address, login_timestamp) FROM data/guest_traveler.csv csv header;
+-- \copy Hotel() FROM data/room.csv csv header;
+\copy Hotel(hotel_id, hotel_name, location, pool, rating, cheapest_nightly_rate, total_rooms) FROM data/hotel.csv csv header;
+\copy Room(room_number, hotel_id, room_capacity, nightly_rate, room_available) FROM data/room.csv csv header;
 
 -- trigger 
-DROP FUNCTION IF EXISTS fn_update_cheapest_nightly_rate() cascade;
+-- DROP FUNCTION IF EXISTS fn_update_cheapest_nightly_rate() cascade;
 CREATE FUNCTION fn_update_cheapest_nightly_rate()
 RETURNS trigger
 LANGUAGE plpgsql AS 
@@ -42,7 +44,7 @@ BEGIN
 END
 $$;
 
-DROP TRIGGER IF EXISTS tr_update_cheapest_nightly_rate ON Room;
+-- DROP TRIGGER IF EXISTS tr_update_cheapest_nightly_rate ON Room;
 
 CREATE TRIGGER tr_update_cheapest_nightly_rate
   -- TODO
