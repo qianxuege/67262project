cc1ccb8e7458d1b77d79b7780851c7562c051fbb
qianxuege <qianxuege15@gmail.com>
2024-12-05 21:09:25 -0500

    updated show_all.sql

--- show_all.sql
+++ show_all.sql
@@ -1,6 +1,30 @@
 \c project
 
-\echo
+
+\echo 'Traveler'
+SELECT * 
+FROM Traveler;
+
+\echo 'Guest_Traveler'
+SELECT * 
+FROM Guest_Traveler;
+
+\echo 'Registered_Traveler'
+SELECT * 
+FROM Registered_Traveler;
+
+\echo 'Aircraft'
+SELECT * 
+FROM Aircraft;
+
+\echo 'Flight'
+SELECT * 
+FROM Flight;
+
+\echo 'Selected_Flight'
+SELECT * 
+FROM Selected_Flight;
+
 \echo 'Hotel'
 SELECT * 
 FROM Hotel;
@@ -8,3 +32,7 @@ FROM Hotel;
 \echo 'Room'
 SELECT * 
 FROM Room;
+
+\echo 'Selected_Hotel'
+SELECT * 
+FROM Selected_Hotel;
